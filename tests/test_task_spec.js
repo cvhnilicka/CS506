43f0c5c99038cca29ee8322792ee6478bcbca150
Kevin Gao <Kevin Gao>
2017-03-12 21:39:15 -0500

    Added test to get by category and improved tests rules for the other 2.

--- tests/test_task_spec.js
+++ tests/test_task_spec.js
@@ -1,15 +1,71 @@
 var request = require("request");
-var ourApp = require ("../server.js");
-var baseUrl = "http://localhost:8000/planner/";
+var server = require("../server.js")
+var base_url = "http://localhost:8000/planner/"
 
-describe("testing tasks", function() {
-    describe("testing get all tasks", function() {
-        it("returns status code 200", function(done) {
-           request.get(baseUrl + 'tasks', function(error, response, body){
-               expect(response.statusCode.toBe(200));
-               ourApp.closeServer();
-               done();
-           })
+describe("tasks", function() {
+    describe("GET tasks /", function() {
+        it("get all tasks for a specific user returns status code 200", function(done) {
+            request.get(base_url + 'tasks/user1', function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it("returns a list of tasks for a specific user", function(done) {
+            request.get(base_url + 'tasks/user1', function(error, response, body) {
+                var correct = true;
+
+                var data = JSON.parse(body);
+
+                for(var i = 0; i < data.length; i++) {
+                    var owner = data[i].owner;
+                    if(owner.username !== 'user1'){
+                        correct = false;
+                    }
+                }
+
+                expect(true).toBe(correct);
+                done();
+            });
+        });
+
+        it("returns status code 200 for retrieving a specific task", function(done) {
+            request.get(base_url + 'task/task1', function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it("returns a specific task", function(done) {
+            request.get(base_url + 'task/task1', function(error, response, body) {
+                expect(body[0].id).toBe(1);
+                done();
+            });
+        });
+
+        it("returns 200 when trying to get all tasks by priority", function(done) {
+            request.get(base_url + 'task/1', function(error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it("returns a specific task by priority", function(done) {
+            request.get(base_url + 'task/1', function(error, response, body) {
+                var correct = true;
+
+                var data = JSON.parse(body);
+
+                for(var i = 0; i < data.length; i++) {
+                    var task = data[i];
+                    if(task.priority !== 1){
+                        correct = false;
+                    }
+                }
+
+                expect(true).toBe(correct);
+                done();
+            });
         });
     });
 });
